87ff98ed77953c44126cd1d74751352fd4c5840c
Daniel Cañadillas <dcanadillas001@ikasle.ehu.eus>
2020-06-01 12:13:48 +0200

    Cambio de rango en getMatrix pasado a forma abreviada con una matriz en vez de dos

--- Shading/projectiveTexture.cc
+++ Shading/projectiveTexture.cc
@@ -13,13 +13,26 @@ ProjectiveTexture::ProjectiveTexture(const std::string &tname, const std::string
 Trfm3D &ProjectiveTexture::getMatrix(){
     Camera *projCam = CameraManager::instance()->find(this->m_camName);
     //Mts [-1,1] -> [0,1]  (s+1)/2
-    Trfm3D mS;
+    //Ms - S/2
+   /* Trfm3D mS;
     mS.setScale(0.5);
     m_projectionMatrix = Trfm3D(mS);
 
+    //Mt - S+1
     Trfm3D mT;
     mT.setTrans(Vector3(1,1,1)); 
     m_projectionMatrix.add(mT);
+    */
+    //Como es en post la matriz resultante seria Ms * Mt * P = (P+1)/2
+    //Sería lo mismo que hacer Mt(1/2) * Ms(1/2) - Mt * Ms * P = P/2 + 1/2
+
+    Trfm3D mTs;
+    mTs.addTrans(Vector3(0.5,0.5,0.5));
+    mTs.addScale(0.5);
+    m_projectionMatrix = Trfm3D(mTs);
+
+
+
 
     //Mp
     m_projectionMatrix.add(projCam->projectionTrfm());
